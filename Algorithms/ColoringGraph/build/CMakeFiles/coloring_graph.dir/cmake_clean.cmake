fd18088e0ee5f1a49aa9156f70fd90413ee6b58d
Joagz <joagomez.dev@gmail.com>
2025-05-16 07:26:02 -0300

    adding ColoringGraph

---
+++ Algorithms/ColoringGraph/build/CMakeFiles/coloring_graph.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coloring_graph.dir/src/main.cpp.o"
+  "CMakeFiles/coloring_graph.dir/src/main.cpp.o.d"
+  "bin/coloring_graph"
+  "bin/coloring_graph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coloring_graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Algorithms/ColoringGraph/build/CMakeFiles/coloring_graph.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/coloring_graph.dir/src/main.cpp.o: \
+ /home/joagz/Desktop/Courses/Algorithms/ColoringGraph/src/main.cpp \
+ /usr/include/stdc-predef.h
